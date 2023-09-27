a88a4c93d19359f144cccaa61391b2844df0b6e0
Md. Zayed Oyshik <zayedoyshik1@gmail.com>
2023-09-27 20:50:56 +0600

    fixed removeTransaction bug

--- lib/utils/transactionModel.dart
+++ lib/utils/transactionModel.dart
@@ -16,35 +16,42 @@ class TransactionModel extends ChangeNotifier {
     return transactions;
   }
 
+  double getMonthlyEarning(){
+    final DateTime now = DateTime.now();
+    final int currentYear = now.year;
+    final int currentMonth = now.month;
+    return transactions
+      .where((transaction) =>
+          transaction[2] == false &&
+          transaction[6].year == currentYear &&
+          transaction[6].month == currentMonth)
+      .map((transaction) =>
+          double.tryParse(transaction[3].toString()) ?? 0.0)
+      .fold(0, (a, b) => a + b);
+  }
+  double getMonthlyExpense(){
+    final DateTime now = DateTime.now();
+    final int currentYear = now.year;
+    final int currentMonth = now.month;
+    return transactions
+      .where((transaction) =>
+          transaction[2] == true &&
+          transaction[6].year == currentYear &&
+          transaction[6].month == currentMonth)
+      .map((transaction) =>
+          double.tryParse(transaction[3].toString()) ?? 0.0)
+      .fold(0, (a, b) => a + b);
+  }
+
   void addTransaction(List<dynamic> transaction) {
     _transactionBox.add(transaction);
     transactions.add(transaction); // Add the transaction to the list
 
     // Update the earning or expense in the user profile based on the boolean value
     var profileData = _profileBox.get("profile_key");
-    // if (profileData != null) {
-    // }
-    final DateTime now = DateTime.now();
-    final int currentYear = now.year;
-    final int currentMonth = now.month;
 
-    final double monthlyExpense = transactions
-    .where((transaction) =>
-        transaction[2] == true &&
-        transaction[6].year == currentYear &&
-        transaction[6].month == currentMonth)
-    .map((transaction) =>
-        double.tryParse(transaction[3].toString()) ?? 0.0)
-    .fold(0, (a, b) => a + b);
-
-    final double monthlyEarning = transactions
-    .where((transaction) =>
-        transaction[2] == false &&
-        transaction[6].year == currentYear &&
-        transaction[6].month == currentMonth)
-    .map((transaction) =>
-        double.tryParse(transaction[3].toString()) ?? 0.0)
-    .fold(0, (a, b) => a + b);
+    final double monthlyExpense = getMonthlyExpense();
+    final double monthlyEarning = getMonthlyEarning();
 
       bool isExpense = transaction[2] == true;
 
@@ -72,34 +79,40 @@ class TransactionModel extends ChangeNotifier {
   void removeTransaction(int index) {
     // Get the transaction being removed
     var removedTransaction = transactions[index];
-    if (removedTransaction != null) {
-      // Update the earning or expense in the user profile based on the boolean value
-      var profileData = _profileBox.get("profile_key");
-      if (profileData != null) {
-        var prevEarning = profileData["earning"];
-        var prevExpense = profileData["expense"];
-        var transactionAmount = double.tryParse(removedTransaction[3] ?? '0') ?? 0.0;
-        bool isExpense = removedTransaction[2] == true;
-
-        if (isExpense) {
-          _profileBox.put("profile_key", {
-            ...profileData,
-            "expense": prevExpense - transactionAmount,
-          });
-        } else {
-          _profileBox.put("profile_key", {
-            ...profileData,
-            "earning": prevEarning - transactionAmount,
-          });
-        }
-      }
-    }
-
     // Remove the transaction from the transactions list
     transactions.removeAt(index);
     // Remove the transaction from the transactions box
     _transactionBox.deleteAt(index);
 
+    if (removedTransaction != null) {
+      // Update the earning or expense in the user profile based on the boolean value
+      var profileData = _profileBox.get("profile_key");
+      if (profileData != null) {
+        var prevEarning = profileData["totalEarning"];
+        var prevExpense = profileData["totalExpense"];
+
+        final double monthlyExpense = getMonthlyExpense();
+        final double monthlyEarning = getMonthlyEarning();
+
+        var transactionAmount = double.tryParse(removedTransaction[3] ?? '0') ?? 0.0;
+
+        bool isExpense = removedTransaction[2] == true;
+
+        if (isExpense) {
+          _profileBox.put("profile_key", {
+            ...profileData,
+            "monthlyExpense": monthlyExpense,
+            "totalExpense": prevExpense - transactionAmount,
+          });
+        } else {
+          _profileBox.put("profile_key", {
+            ...profileData,
+            "monthlyEarning": monthlyEarning,
+            "totalEarning": prevEarning - transactionAmount,
+          });
+        }
+      }
+    }
     notifyListeners();
   }
 }
